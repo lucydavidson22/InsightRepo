3cf6df3222a360a650578dfa1bd1d42c31b31ef6
Lucy Davidson <lucy.davidson@outlook.com>
2023-04-13 10:07:19 -0400

    initial 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { MciListComponent } from './mci/mci-list/mci-list.component';
 import { MciComponent } from './mci/mci.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MciListComponent,
-    MciComponent
+    MciComponent,
   ],
   imports: [
     BrowserModule
