b2c30d5c54e2a9d6234d37a41224fc1d049c8499
Lucy Davidson <lucy.davidson@outlook.com>
2023-04-13 09:37:58 -0400

    initial commit

---
+++ src/app/mci-list/mci-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mci-list',
+  templateUrl: './mci-list.component.html',
+  styleUrls: ['./mci-list.component.css']
+})
+export class MciListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
